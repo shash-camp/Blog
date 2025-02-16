25cf71d44e8894f4d2cf03ea879cf1ce133c5dc2
shash-camp <shashank221405@gmail.com>
2025-02-16 21:25:00 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,10 @@ import { AuthLayout, Login } from './components/index.js'
 import AddPost from "./pages/AddPost";
 import Signup from './pages/Signup'
 import EditPost from "./pages/EditPost";
-import AllPosts from "./pages/Allposts";
+
 import Post from "./pages/Post";
 
+import AllPosts from "./pages/Allposts.jsx";
 
 const router = createBrowserRouter([
   {
@@ -83,4 +84,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>,
 )
-
